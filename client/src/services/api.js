bfcce60d5b49e6b1e6f05b18feca88bc6bdc6fa9
xiebingtang <xiebingtang@ideadata.com.cn>
2019-05-15 11:59:33 +0800

    axios的再次封装，简化api接口的书写

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,13 @@
-import axios from 'axios'
+// import axios from 'axios'
+import request from '@/utils/request'
 export async function reqUser(){
-    return new Promise((resolve, reject) => {
-       axios.get('/api/users').then((response)=>{
-           console.log('response', response)
-           resolve(response.data)
-       }).catch((error)=>{
-           reject(error)
-       })
-    })
+    // return new Promise((resolve, reject) => {
+    //    axios.get('/api/users').then((response)=>{
+    //        console.log('response', response)
+    //        resolve(response.data)
+    //    }).catch((error)=>{
+    //        reject(error)
+    //    })
+    // })
+    return request('/api/users')
 }
